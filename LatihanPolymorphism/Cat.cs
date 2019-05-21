14802435eda5664d678b1f2265794ea107115b06
ratutriaarospurnama <ratutriaarospurnama19@gmail.com>
2019-05-21 12:14:58 +0700

    LatihanPolymorphism

---
+++ LatihanPolymorphism/Cat.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanPolymorphism
+{
+    public class Cat : Animal
+    {
+        public override void Speak()
+        {
+            Console.WriteLine("Cat says: Meong-meong");
+        }
+    }
+}
